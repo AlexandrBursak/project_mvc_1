5ef8c68341fe94e9244ae1ac78252d1037657c42
RomaSlobodeniuk <romsl1991@gmail.com>
2016-10-21 16:44:04 +0300

    roma added some changes in layout and index_about

--- application/templates/components/layout.php
+++ application/templates/components/layout.php
@@ -2,6 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $meta_data['title']; ?></title>
 
     <link href="<?php echo $permalink; ?>assets/bootstrap/css/bootstrap.css" rel="stylesheet">
